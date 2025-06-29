790b31030a84e4e48d90ba9577b4a287df8bc6b4
Massatrio16 <dimassetosatrio@gmail.com>
2025-06-29 07:02:28 +0700

    add pbrp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,7 +6,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_S681LN.mk
-
+    $(LOCAL_DIR)/twrp_S681LN.mk \
+    $(LOCAL_DIR)/pb_S681LN.mk
 COMMON_LUNCH_CHOICES := \
-    twrp_S681LN-eng 
+    twrp_S681LN-eng \
+    pb_S681LN-eng 
